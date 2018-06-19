7d89a9677cdf06d9aa4ba1bc4545473fd7c801e2
Penthious <tleffew1994@gmail.com>
2018-06-19 01:21:23 -0600

    updated errors

--- src/utils/yupSchemas.ts
+++ src/utils/yupSchemas.ts
@@ -4,6 +4,8 @@ import {
   emailNotLongEnough,
   invalidEmail,
   passwordNotLongEnough,
+  passwordRequired,
+  emailRequired,
 } from "../modules/user/register/errorMessages";
 import { missingPasswordKeyError } from "../modules/user/forgotPassword/errorMessages";
 
@@ -11,12 +13,14 @@ export const emailValidation = yup
   .string()
   .min(3, emailNotLongEnough)
   .max(255)
-  .email(invalidEmail);
+  .email(invalidEmail)
+  .required(emailRequired);
 
 export const passwordValidation = yup
   .string()
   .min(3, passwordNotLongEnough)
-  .max(255);
+  .max(255)
+  .required(passwordRequired);
 
 export const newPasswordKeyValidation = yup
   .string()
